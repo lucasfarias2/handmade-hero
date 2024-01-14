700f13da39e00f23c98d2203e22b3982e7a90021
lucasfarias2 <lukf95@gmail.com>
2024-01-14 16:29:30 +0100

    Window rendering

--- Handmade.cpp
+++ Handmade.cpp
@@ -1,8 +1,107 @@
 #include <windows.h>
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
 {
-	MessageBoxA(0, "This is Handmade Hero!", "Handmade Hero", MB_OK|MB_ICONINFORMATION);
+	LRESULT Result = 0;
+
+	switch (Message)
+	{
+		case WM_SIZE:
+		{
+			OutputDebugStringA("WM_SIZE\n");
+		} break;
+
+		case WM_DESTROY:
+		{
+			OutputDebugStringA("WM_DESTROY\n");
+		} break;
+
+		case WM_CLOSE:
+		{
+			OutputDebugStringA("WM_CLOSE\n");
+		} break;
+
+		case WM_ACTIVATEAPP:
+		{
+			OutputDebugStringA("WM_ACTIVATEAPP\n");
+		} break;
+
+		case WM_PAINT:
+		{
+			PAINTSTRUCT Paint;
+			HDC DeviceContext = BeginPaint(Window, &Paint);
+			int X = Paint.rcPaint.left;
+			int Y = Paint.rcPaint.top;
+			int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+			PatBlt(DeviceContext, X, Y, Width, Height, WHITENESS);
+			EndPaint(Window, &Paint);
+		} break;
+
+		default:
+		{
+			Result = DefWindowProc(Window, Message, WParam, LParam);
+		} break;
+	}
+
+	return(Result);
+}
+
+int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLine, int ShowCode)
+{
+
+	WNDCLASS WindowClass = {};
+
+	WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+	WindowClass.lpfnWndProc = MainWindowCallback;
+	WindowClass.hInstance = Instance;
+	WindowClass.lpszClassName = L"HandmadeHeroWindowClass";
+
+	if (RegisterClass(&WindowClass))
+	{
+		HWND WindowHandle =
+			CreateWindowEx(
+				0,
+				WindowClass.lpszClassName,
+				L"Handmade Hero",
+				WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+				CW_USEDEFAULT,
+				CW_USEDEFAULT,
+				CW_USEDEFAULT,
+				CW_USEDEFAULT,
+				0,
+				0,
+				Instance,
+				0
+			);
+
+		if (WindowHandle)
+		{
+			MSG Message;
+			for (;;)
+			{
+				BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+				if (MessageResult > 0)
+				{
+					TranslateMessage(&Message);
+					DispatchMessage(&Message);
+				}
+				else
+				{
+					break;
+				}
+			}
+
+		}
+		else
+		{
+
+		}
+
+	}
+	else
+	{
+	}
 
 	return(0);
 }

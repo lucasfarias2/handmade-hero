09315bea4395d610ed45718d16ad1a44f4ba17da
lucasfarias2 <lukf95@gmail.com>
2024-01-14 19:00:14 +0100

    Window resize and update

--- Handmade.cpp
+++ Handmade.cpp
@@ -1,6 +1,46 @@
 #include <windows.h>
 
-LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
+#define internal static
+#define local_persist static
+#define global_variable static
+
+global_variable bool Running = false;
+
+internal void Win32ResizeDIBSection(int Width, int Height)
+{
+	BITMAPINFO BitmapInfo;
+	BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+	BitmapInfo.bmiHeader.biWidth = Width;
+	BitmapInfo.bmiHeader.biHeight = Height;
+	BitmapInfo.bmiHeader.biPlanes = 1;
+	BitmapInfo.bmiHeader.biBitCount = 32;
+	BitmapInfo.bmiHeader.biCompression = BI_RGB;
+	BitmapInfo.bmiHeader.biSizeImage = 0;
+	BitmapInfo.bmiHeader.biXPelsPerMeter = 0;
+	BitmapInfo.bmiHeader.biYPelsPerMeter = 0;
+	BitmapInfo.bmiHeader.biClrUsed = 0;
+	BitmapInfo.bmiHeader.biClrImportant = 0;
+
+	void *BitmapMemory;
+	HBITMAP BitmapHandle = CreateDIBSection(
+		DeviceContext, &BitmapInfo,
+		DIB_RGB_COLORS, 0, 0);
+
+}
+
+internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+{
+	StrechtDIBits(DeviceContext,
+		X, Y, Width, Height,
+		X, Y, Width, Height,
+		const VOID * lpBits,
+		const BITMAPINFO * lpBitsInfo,
+		DIB_RGB_COLORS,
+		SRCCOPY
+	);
+}
+
+LRESULT CALLBACK Win32MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
 {
 	LRESULT Result = 0;
 
@@ -8,17 +48,25 @@ LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LP
 	{
 		case WM_SIZE:
 		{
+			RECT ClientRect;
+
+			GetClientRect(Window, &ClientRect);
+
+			int Height = ClientRect.bottom - ClientRect.top;
+			int Width = ClientRect.right - ClientRect.left;
+
+			Win32ResizeDIBSection(Width, Height);
 			OutputDebugStringA("WM_SIZE\n");
 		} break;
 
 		case WM_DESTROY:
 		{
-			OutputDebugStringA("WM_DESTROY\n");
+			Running = false;
 		} break;
 
 		case WM_CLOSE:
 		{
-			OutputDebugStringA("WM_CLOSE\n");
+			Running = false;
 		} break;
 
 		case WM_ACTIVATEAPP:
@@ -34,6 +82,9 @@ LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LP
 			int Y = Paint.rcPaint.top;
 			int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
 			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+
+			Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
+
 			PatBlt(DeviceContext, X, Y, Width, Height, WHITENESS);
 			EndPaint(Window, &Paint);
 		} break;
@@ -53,7 +104,7 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLi
 	WNDCLASS WindowClass = {};
 
 	WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
-	WindowClass.lpfnWndProc = MainWindowCallback;
+	WindowClass.lpfnWndProc = Win32MainWindowCallback;
 	WindowClass.hInstance = Instance;
 	WindowClass.lpszClassName = L"HandmadeHeroWindowClass";
 
@@ -77,8 +128,9 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLi
 
 		if (WindowHandle)
 		{
+			Running = true;
 			MSG Message;
-			for (;;)
+			while (Running)
 			{
 				BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
 				if (MessageResult > 0)
